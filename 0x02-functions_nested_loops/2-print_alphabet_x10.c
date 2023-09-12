bd3ef58d9c147e1e211c506c0094dc7e5dee9bca
Bilalnej <bilal.nejjara14@gmail.com>
2023-09-12 16:36:31 -0700

    second

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* print_alphabet_x10 - print alphabet
+*
+* Return: Always 0.
+*/
+void print_alphabet_x10(void)
+{
+int i = 0;
+char a;
+while (i < 10)
+{
+for (a = 'a'; a <= 'z'; a++)
+{
+_putchar(a);
+}
+_putchar('\n');
+i++;
+}
+}
