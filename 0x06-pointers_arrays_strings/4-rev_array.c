9491bab1cfbf72480844d1a4df4ac1e57ef89cc4
Bilalnej <bilal.nejjara14@gmail.com>
2023-10-04 15:33:41 -0700

    first

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *reverse_array - revereses an array.
+ *@a: pointer to array.
+ *@n: number of elements of an array.
+ *
+ *Return: void.
+ */
+void reverse_array(int *a, int n)
+{
+	int temp, counter;
+
+	n = n - 1;
---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ *string_toupper - changes all lowercase letters of a string to uppercase.
+ *@x: pointer to string.
+ *
+ *Return: pointer to uppercase string.
+ */
+char *string_toupper(char *x)
+{
+	int length;
+
+	length = 0;
